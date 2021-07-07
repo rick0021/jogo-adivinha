4cd36bd06f85e40371b2700255db92c3900a2327
caue <cziinff01@gmail.com>
2021-07-07 00:34:09 +0000

    adiciona jogar novamente

--- script.js
+++ script.js
@@ -2,10 +2,28 @@ var min=0
 var max=10
 var numero = Math.random() * (max - min) + min;
 numero = Math.ceil(numero)
-var resposta = Number(prompt("entre com um numero(de 1 ate "+ max))
-if (resposta == numero) {
-   alert("voce acertou") 
-}else{
-    alert("voce errou")
+var resposta =0
+
+
+
+var resposta_usuario="y"
+var jogar= true
+while(jogar==true){
+    resposta = Number(prompt("entre com um numero(de 1 ate "+ max))
+    if (resposta == numero) {
+        alert("voce acertou") 
+    }else{
+         alert("voce errou")
+    }
+    alert(numero)
+    
+    
+    resposta_usuario= prompt("jogar de novo?(y/n)")
+    if(resposta_usuario=="y"){
+        jogar=true
+    }else{
+        jogar=false
+    }
+
 }
-alert(numero)
+alert("ate mais")
